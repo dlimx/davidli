14b6859cfb11ff5250035bf4b416df52ecc85cc3
dlimx <dlimx@users.noreply.github.com>
2021-01-06 08:11:30 +0000

    deploy: 9ac836726f3f464f0256cdb35c4eae3a966bbeef

--- component---src-pages-about-tsx-1b97273086425b08e24d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"2l/u":function(n,u,t){"use strict";t.r(u);t("q1tI");var e=t("4XLq"),o=t("ijqZ"),l=t("qKvR");u.default=function(){return Object(l.jsx)(e.a,null,Object(l.jsx)(o.a,null,Object(l.jsx)("h1",null,"You are here!"),Object(l.jsx)("h2",null,"But nothing found for you #404")))}}}]);
-//# sourceMappingURL=component---src-pages-about-tsx-1b97273086425b08e24d.js.map
