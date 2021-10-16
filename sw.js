91ad982657ec8d46b9ba9794d71abbb62b8c4b41
dlimx <dlimx@users.noreply.github.com>
2021-10-16 20:45:51 +0000

    deploy: 8bafb46ef6857fcd4b97c98b97f3a3500cc62aea

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-160c14886e69c6e25d2e.js"
+    "url": "webpack-runtime-18f4f1559917c571a5f6.js"
   },
   {
     "url": "styles.ae777237ee7ff3ddac78.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f18c1606de5ff9f906c946eeddd28295"
+    "revision": "e170098066b8b9599ee99ab444199b1a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
