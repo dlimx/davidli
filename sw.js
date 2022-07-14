9d721172e25079a6fd0b96520817e026d5a90f1b
dlimx <me@davidli.io>
2022-07-14 01:42:33 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-55fca25eed458b4b3a18.js"
+    "url": "webpack-runtime-8e1c9599909e2171d7a0.js"
   },
   {
     "url": "styles.ae777237ee7ff3ddac78.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c4dda683b3049746e4259e6b96283ba5"
+    "revision": "5aba0784864708f079ccf98cc671b1c6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
